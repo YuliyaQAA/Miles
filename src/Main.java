feb95d3085627697d89f2736d0ef69b1461e33ea
YuliyaQAA <smolnikova.biz@gmail.com>
2022-07-14 18:12:38 +0400

    Initail Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 8000;
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+        System.out.println(miles);
+    }
+}
